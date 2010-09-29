48a47c8c0f2ac7cf84c37e3335d314102a20af1e
promontis <mvb@promontis.nl>
2010-09-29 23:06:40 +0200

    Added missing classes

---
+++ Main/Hayman.Domain/MetaAssociation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ncqrs.Domain;
+using Hayman.Events;
+
+namespace Hayman.Domain
+{
+    public class MetaAssociation : AggregateRootMappedByConvention
+	{
+        public Guid MetaAssociationId { set; get; }
+        public Guid MetaitemSourceId { get; set; }
+        public Guid MetaitemTargetId { get; set; }
+
+        public MetaAssociation(Guid metaAssociationId, Guid metaitemSourceId, Guid metaitemTargetId)
+            : base(metaAssociationId)
+		{
+            ApplyEvent(new MetaAssociationCreated(metaAssociationId, metaitemSourceId, MetaitemTargetId));
+		}
+
+        private void OnMetaAssociationCreated(MetaAssociationCreated e)
+		{
+			MetaAssociationId = e.MetaAssociationId;
+			MetaitemSourceId = e.MetaitemSourceId;
+            MetaitemTargetId = e.MetaitemTargetId;
+		}
+	}
+}

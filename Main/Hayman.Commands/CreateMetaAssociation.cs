8b9a89034e8c1084ecacc999ee395fbeca085961
promontis <mvb@promontis.nl>
2010-10-24 20:00:03 +0200

    Added initial MetaAssociation support

--- Main/Hayman.Commands/CreateMetaAssociation.cs
+++ Main/Hayman.Commands/CreateMetaAssociation.cs
@@ -5,16 +5,14 @@ namespace Hayman.Commands
 {
 	[Serializable]
 	//[MapsToAggregateRootMethod(typeof(MetaModel))]
-	public class AddMetaAssociation : CommandBase
+	public class CreateMetaAssociation : CommandBase
 	{
-        public Guid MetaModelId { get; private set; }
         public Guid MetaAssociationId { get; private set; }
         public Guid MetaitemSourceId { get; private set; }
         public Guid MetaitemTargetId { get; private set; }
 
-        public AddMetaAssociation(Guid metaModelId, Guid metaAssociationId, Guid metaitemSourceId, Guid metaItemTargetId)
+        public CreateMetaAssociation(Guid metaModelId, Guid metaAssociationId, Guid metaitemSourceId, Guid metaItemTargetId)
 		{
-            MetaModelId = metaModelId;
             MetaAssociationId = metaAssociationId;
             MetaitemSourceId = metaitemSourceId;
 			MetaitemTargetId = metaItemTargetId;
---
+++ Main/Hayman.Events/MetaAssociationCreated.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ncqrs.Eventing.Sourcing;
+
+namespace Hayman.Events
+{
+	public class MetaAssociationCreated : SourcedEvent
+	{
+        public Guid MetaAssociationId { get; set; }
+        public Guid MetaitemSourceId { get; set; }
+        public Guid MetaitemTargetId { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the MetaAssociationCreated class.
+        /// </summary>
+        public MetaAssociationCreated(Guid metaAssociationId, Guid metaitemSourceId, Guid metaitemTargetId)
+        {
+            MetaAssociationId = metaAssociationId;
+            MetaitemSourceId = metaitemSourceId;
+            MetaitemTargetId = metaitemTargetId;
+        }
+    }
+}
